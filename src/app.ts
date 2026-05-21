62b3093bd4d22c627107adc530345bc32d680e11
abdullahalnoman003 <noman22622@gmail.com>
2026-05-21 23:23:00 +0600

    Updated app.ts file with  auth path and updated config path

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
+import cookieParser from "cookie-parser";
 import express, { type Request, type Response } from "express";
+import { authRoute } from "./modules/auth/auth.route";
 const app = express()
 
+// Middlewares
+app.use(express.json());
+// app.use(cookieParser());
+
+
+//Default route or root route
 app.get('/', (req : Request, res: Response) => {
-  res.send('Hello World!');
-  console.log("This Is from App ")
+  res.status(200 ).json({
+    message : "Welcome to DevIntel Server",
+  })  
 })
 
+
+// Api Routes
+app.use("/api/auth/", authRoute)
 export default  app ;
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,5 +7,9 @@ dotenv.config({
 
 const config = {
     port : process.env.PORT,
+    connection : process.env.CONNECTION,
+    secret : process.env.ACCESS_TOKEN_SECRET,
+    refresh_secret: process.env.REFRESH_TOKEN_SECRET,
+    bycrypt_round : Number( process.env.BYCR_ROUND)
 }
 export default config;
