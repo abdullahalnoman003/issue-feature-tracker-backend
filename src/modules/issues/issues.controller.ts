d41169b49dacdf5a5216f79e81fb4c24fabad5b6
abdullahalnoman003 <noman22622@gmail.com>
2026-05-23 00:33:21 +0600

    added issue controller methods for improved structure and type safety, and operation logic

--- src/modules/issues/issues.controller.ts
+++ src/modules/issues/issues.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { issueServices } from "./issues.service";
 import responseSender from "../../utility/responseSender";
 import { AppError } from "../../middleware/globalErrorhandler";
+import type { IIUsers } from "./issues.interface";
 
 const createReport = async (req: Request, res: Response) => {
   if (!req.user) {
@@ -23,20 +24,42 @@ const createReport = async (req: Request, res: Response) => {
   });
 };
 const getAllReport = async (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "API OK"
-    })
+  res.status(200).json({
+    message: "API OK",
+  });
 };
-const getSingleReport = async (req: Request, res : Response) => {
-    const {id }= req.params;
-    const result = await issueServices.getSingleIssueFromDB(id as string);
-    res.status(200).json({
-        success: true,
-        data : result,
-    })
+const getSingleReport = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await issueServices.getSingleIssueFromDB(id as string);
+  res.status(200).json({
+    success: true,
+    data: result,
+  });
 };
-const updateSingleReport = async () => {};
-const deleteSingleReport = async () => {};
+const updateSingleReport = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = req.user!;
+  const result = await issueServices.updateIssueFromDB(
+    req.body,
+    user as IIUsers,
+    id as string,
+  );
+  responseSender(res, {
+    statusCode: 200,
+    success: true,
+    message: "Issue Updated Successfully!",
+    data: result,
+  });
+};
+const deleteSingleReport = async (req: Request, res: Response) => {
+  const { id } = req.params;
+ const result = await issueServices.deleteIssueFromDB(id as string);
+ console.log(result);
+  res.status(200).json({
+    success: true,
+    message: "Issue deleted successfully",
+  });
+};
 export const issuesController = {
   createReport,
   getAllReport,
