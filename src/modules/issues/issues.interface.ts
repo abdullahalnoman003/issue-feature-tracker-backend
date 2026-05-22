b8c17558b3623acdae486340c31b1c4c7e38c5b1
abdullahalnoman003 <noman22622@gmail.com>
2026-05-23 00:33:37 +0600

    Add IIssue and IIUsers interfaces to define issue and user structures

--- src/modules/issues/issues.interface.ts
+++ src/modules/issues/issues.interface.ts
@@ -3,3 +3,16 @@ export interface IUser {
     name: string;
     email: string;
 }
+export interface IIssue {
+    title : string;
+    description : string;
+    type : string;
+}
+export interface IIUsers{
+    id: number;
+    name: string;
+    role: string;
+    iat : number;
+    exp: number;
+
+}
